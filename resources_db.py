00a884824874163bf5df0777c1ca7167fda10e83
ming-lamasherpagit config --global user.email smingma767@gmail.com <smingma767@gmail.com>
2025-06-06 08:27:36 +0545

    check logic for sufficient resources

--- resources_db.py
+++ resources_db.py
@@ -1,6 +1,33 @@
 machine_resources = {
-    "water":500,
-    "milk" :300,
-    "coffee" :100,
-    "money":0
+    "water": 500,
+    "milk": 300,
+    "coffee": 100,
+    "money": 0
+}
+
+COFFEE_MENU = {
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24
+        },
+        "money": 2.5
+    },
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "milk": 0,
+            "coffee": 18
+        },
+        "money": 1.5
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 300,
+            "milk": 125,
+            "coffee": 35
+        },
+        "money": 3.5
+    }
 }
---
+++ utils.py
@@ -0,0 +1,17 @@
+from resources_db import machine_resources as resources
+from resources_db import COFFEE_MENU
+
+def get_resources_report():
+    print(f"Water: {resources.get('water',' ')}ml")
+    print(f"milk: {resources.get('milk',' ')}ml")
+    print(f"coffee: {resources.get('coffee',' ' )}gm")
+    print(f"money: {resources.get('money',' ')}")
+
+def is_resource_sufficient(choice):
+    is_resource_suff = True
+    for ingred,qty in COFFEE_MENU.get(choice,{}).get("ingredients", " ").items():
+        if resources.get(ingred,0) < qty:
+            print(f"we donot have enough {ingred}!!! Try another coffee.")
+            is_resource_suff = False
+    
+    return is_resource_suff
