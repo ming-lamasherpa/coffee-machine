3888b29a0dbcf52de7104b45ea04c3832832d780
ming-lamasherpagit config --global user.email smingma767@gmail.com <smingma767@gmail.com>
2025-06-09 10:54:54 +0545

    Process coin ligic updated

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils import get_resources_report,is_resource_sufficient
+from utils import get_resources_report,is_resource_sufficient,process_coins
 is_machine_on =True
 
 while is_machine_on:
@@ -9,11 +9,13 @@ while is_machine_on:
     elif choice.lower() == "report":
         get_resources_report()
 
+
     else:
         if choice not in ["latte","espresso","cappuccino"]:
             print("sorry !!! {choice} is not available.select correctly ")
         else:
-            print(is_resource_sufficient(choice))
+            if is_resource_sufficient(choice):
+                process_coins(choice)
 
             # print(choice)
     
--- utils.py
+++ utils.py
@@ -2,16 +2,31 @@ from resources_db import machine_resources as resources
 from resources_db import COFFEE_MENU
 
 def get_resources_report():
-    print(f"Water: {resources.get('water',' ')}ml")
-    print(f"milk: {resources.get('milk',' ')}ml")
-    print(f"coffee: {resources.get('coffee',' ' )}gm")
-    print(f"money: {resources.get('money',' ')}")
+    print(f"Water: {resources.get('water', ' ')}ml")
+    print(f"Milk: {resources.get('milk', ' ')}ml")
+    print(f"Coffee: {resources.get('coffee', ' ')}gm")
+    print(f"Money: ${resources.get('money', 0):.2f}")
 
 def is_resource_sufficient(choice):
     is_resource_suff = True
-    for ingred,qty in COFFEE_MENU.get(choice,{}).get("ingredients", " ").items():
-        if resources.get(ingred,0) < qty:
-            print(f"Sorry there is not enough {ingred}!!! ")
-            is_resource_suff = False
-    
+    for ingred, qty in COFFEE_MENU[choice]["ingredients"].items():
+        if resources.get(ingred, 0) < qty:
+            print(f"Sorry, there is not enough {ingred}!")
+            is_resource_suff =  False
+            break
     return is_resource_suff
+
+def process_coins(choice):
+    print("Please Insert coins:")
+    quarters = int(input("How many quarters?: "))   # $0.25
+    dimes = int(input("How many Dimes?: "))      # $0.10
+    nickels = int(input("How many Nickels?: "))    # $0.05
+    pennies = int(input("How many Pennies?: "))    # $0.01
+    total = quarters * 0.25 + dimes * 0.10 + nickels * 0.05 + pennies * 0.01
+    cost = COFFEE_MENU.get(choice,{}).get("money", " ")
+
+    if total > cost:
+        print(f"here is ${round(total - cost, 2)} in charge.🪙")
+        print(f"Here is your {choice}🍵.Enjoy!")
+    else:
+        print("Sorry that's not money.Money refunded.")
